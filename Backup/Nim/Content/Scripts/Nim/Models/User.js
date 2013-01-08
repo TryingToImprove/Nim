f78278492d0b17d51452017d82f22fb77ef3fae8
Oliver Lassen <privat.oliver.lassen@gmail.com>
2013-01-08 15:42:28 +0100

    remove backup solution

--- Backup/Nim/Content/Scripts/Nim/Models/User.js
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="../docs.js" />
-
-define(["$", "Underscore", "Backbone", "Marionette"], function ($, _, Backbone, Marionette) {
-    var Model = Backbone.Model.extend({
-    });
-
-    return Model;
-});
--- Backup/Nim/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nim.Global" Language="C#" %>
